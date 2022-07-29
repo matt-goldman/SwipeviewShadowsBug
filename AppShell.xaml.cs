af1c886208ea9114e0ccbdbbaa7ad695ef8cf7a2
Matt Goldman <matt.goldman@outlook.com>
2022-07-29 16:36:55 +1000

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SwipeviewShadows;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SwipeviewShadows;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
